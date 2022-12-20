de074b70917af5badc25c75357a892c4a5d954f9
user <user@shailendra>
2022-12-20 12:13:54 +0530

    StringBuilder Reverse String

--- SeleniumPractise/src/test/java/javaprograms/ReverseString.java
+++ SeleniumPractise/src/test/java/javaprograms/ReverseString.java
@@ -4,20 +4,16 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String str="Setting";
+	
+		String str="laptop";
 		String nstr="";
 		char ch;
-		
 		for(int i=0;i<str.length();i++)
 		{
-			
 			ch=str.charAt(i);
 			nstr=ch+nstr;
-			
 		}
 		System.out.println(nstr);
-
 	}
 
 }
---
+++ SeleniumPractise/src/test/java/javaprograms/ReverseString_Using_StringBuilder.java
@@ -0,0 +1,16 @@
+package javaprograms;
+
+public class ReverseString_Using_StringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String input="Mixhubb";
+		StringBuilder input1=new StringBuilder();
+		input1.append(input);
+		input1.reverse();
+		System.out.println(input1);
+
+	}
+
+}
