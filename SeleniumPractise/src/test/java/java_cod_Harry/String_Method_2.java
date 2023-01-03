b069302917b8de5b85d5439011662a2b5a216609
user <user@shailendra>
2023-01-03 16:44:34 +0530

    String Method Uppercase,lowercase, find length of string, trim function

--- SeleniumPractise/src/test/java/java_cod_Harry/String_Method_2.java
+++ SeleniumPractise/src/test/java/java_cod_Harry/String_Method_2.java
@@ -15,17 +15,40 @@ public class String_Method_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Find length of the string
+		
 		String name ="Shailendra singh";
 		int findlength=name.length();
 		
 		System.out.println(findlength);
+		System.out.println("-----------------------");
 	
 		// Remove space count
 		System.out.println(name.replace(" ", "").length());
 		
+		// String LowerCase And UpperCase
+		
 		String lc= "TESTING demo";
 		System.out.println(lc.toLowerCase());
 		System.out.println(lc.toUpperCase());
+		
+		System.out.println("-----------------------");
+		
+		// Trim is used for remove space
+		
+		String nontrimstring="           Shailendra            ";
+		System.out.println(nontrimstring);
+		String trimmedstring=nontrimstring.trim();
+		System.out.println(trimmedstring);
+		System.out.println(nontrimstring.trim());
+		
+		System.out.println("-----------------------");
+		
+		// Substring start index matlab hum jaha se number dalenge waha se start hoga 
+		// Example shailendra ab me substring me 4 add karta hu to l se start hoga word
+		
+		String substring="Shailendra";
+		System.out.println(substring.substring(4));
 
 	}
 
